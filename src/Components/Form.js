d74f3e79933c927259218e804e6eec972788db6c
“sampritila” <sampath.pritila@gmail.com>
2020-01-22 15:32:39 +0000

    sam from i7

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,7 +9,7 @@ class Form extends React.Component{
                 <form onSubmit={this.props.getWeather}>
                     
                     <input type="text" name="city" placeholder="City......."/>
-                    <input type="text" name="country" placeholder="Country........"/>
+                    <input type="text" name="country" placeholder="Country......"/>
                     <button>Get Weather</button>
                 </form>
 
